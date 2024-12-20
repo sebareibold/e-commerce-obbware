984ce71e6a4a4abb6b9362396cf9abcc5497ca18
Sebastian Reibold <sebastian.reibold@est.fi.uncoma.edu.ar>
2024-12-20 12:13:23 -0300

    Subiendolo a git

---
+++ proyecto-coderhouse/src/Componentes/Navbar/Navbar.jsx
@@ -0,0 +1,23 @@
+
+import React from "react";
+import './Navbar.css'
+
+
+const NavBar = () => {
+
+    return (
+        <>
+        <header>
+            <h1>Tienda Hardware</h1>
+            <nav>
+                <li>Home</li>
+                <li>Productos</li>
+                <li>Sobre nosotros</li>
+            </nav>
+        </header>
+
+        </>
+    );
+}
+export default NavBar
+
