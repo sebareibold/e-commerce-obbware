6e0c55130408ccfb87af36f45b3af3382afd6cd0
Sebastian Reibold <sebastian.reibold@est.fi.uncoma.edu.ar>
2024-12-21 13:34:14 -0300

    CreaTuLanding+Reibold

--- proyecto-coderhouse/src/Componentes/App.jsx
+++ proyecto-coderhouse/src/Componentes/App.jsx
@@ -4,7 +4,7 @@ import Navbar from './Navbar/Navbar'
 import ItemListContainer from './ItemListContainer/ItemListContainer'
 
 function App() {
-
+  {/*Primer entrega*/}
   const ItemListInstant = "Lista de Productos";
 
   return (
